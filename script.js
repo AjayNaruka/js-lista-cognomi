a00e116267307d2aa2401b25d04d4741e031b833
AjayNaruka <naruka.ajay1997@gmail.com>
2021-04-12 14:15:28 +0200

    html-css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var surnameList = ["Bianchi","Neri","Rossi","Verdi","Gialli"];
 var plusList = [...surnameList];
 console.log("Original List:\n" + surnameList)
-var surname = prompt("Inserisci il tuo cognome: ");
+var surname = prompt("Inserisci il tuo cognome ( Case sensitive ): ");
 plusList.push(surname);
 console.log("Add List:\n" + plusList.sort());
 
@@ -13,5 +13,12 @@ while(!found){
     found=true;
   }
   index++;
+}
+//STAMPA HTML
+for(index=0; index<plusList.length;index++){
+  if(plusList[index]!=surname) document.getElementById("lista").innerHTML += "<li>" + plusList[index] +"</li>";
+  else{
+    document.getElementById("lista").innerHTML += "<li class='red'>" + plusList[index] +"</li>";
+  }
 }
 
